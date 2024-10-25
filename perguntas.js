a4b960bec70b836c68af4eb2d34f0a66b9a23ec5
jesuskamila <168206140+jesuskamila@users.noreply.github.com>
2024-10-25 11:53:09 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,17 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+
+criaCartao(
+    'Matemática',
+    'Meu avô tem 5 filhos, cada filho tem 3 filhos. Quantos primos eu tenho??',
+    'Tenho 12 primos (4.3 = 12), porque um dos filhos do meu avô é o meu pai, que tem 3 filhos (eu e meus 2 irmãos).'
+)
+
+)
+
+criaCartao(
+    'História',
+    'Quantos anos durou a guerra de 100 anos?',
+    '106 anos'
+)
