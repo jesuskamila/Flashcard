1ee68f9d956df09cba1630329edcb24c5e4d8206
jesuskamila <168206140+jesuskamila@users.noreply.github.com>
2024-11-21 11:28:27 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -3,19 +3,16 @@ criaCartao(
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
-
 criaCartao(
     'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
-
 criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
-
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
@@ -27,9 +24,49 @@ criaCartao(
     'Meu avô tem 5 filhos, cada filho tem 3 filhos. Quantos primos eu tenho??',
     'Tenho 12 primos (4.3 = 12), porque um dos filhos do meu avô é o meu pai, que tem 3 filhos (eu e meus 2 irmãos).'
 )
-
 criaCartao(
     'História',
     'Quantos anos durou a guerra de 100 anos?',
     '106 anos'
 )
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+criaCartao(
+    'Química',
+    'A que temperatura a água ferve?',
+    '100 °C'
+)
+criaCartao(
+    'Biologia',
+    'Quantos ossos temos no nosso corpo?',
+    '206'
+)
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'História',
+    'Quantos anos durou a guerra de 100 anos?',
+    '106 anos'
+)
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+
+
+
+
+
