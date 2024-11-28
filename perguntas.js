939fdb7c0e1a6385144c5b561209b9e33a18ac4d
jesuskamila <168206140+jesuskamila@users.noreply.github.com>
2024-11-28 11:18:45 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,26 +1,26 @@
 criaCartao(
-    'Programação',
+    'Esporte',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 criaCartao(
-    'Geografia',
+    'Música,
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 criaCartao(
-    'Programação',
+    'Geografia',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 criaCartao(
-    'Lingua inglesa',
+    'História,
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
-    'Matemática',
+    'Animais',
     'Meu avô tem 5 filhos, cada filho tem 3 filhos. Quantos primos eu tenho??',
     'Tenho 12 primos (4.3 = 12), porque um dos filhos do meu avô é o meu pai, que tem 3 filhos (eu e meus 2 irmãos).'
 )
@@ -35,12 +35,12 @@ criaCartao(
     'Monte Everest'
 )
 criaCartao(
-    'Química',
+    'Música,
     'A que temperatura a água ferve?',
     '100 °C'
 )
 criaCartao(
-    'Biologia',
+    'Esporte',
     'Quantos ossos temos no nosso corpo?',
     '206'
 )
@@ -50,7 +50,7 @@ criaCartao(
     'Monte Everest'
 )
 criaCartao(
-    'Programação',
+    'Animais',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
@@ -60,12 +60,41 @@ criaCartao(
     '106 anos'
 )
 criaCartao(
-    'Geografia',
+    'Esporte',
     'Qual a montanha mais alta do mundo?',
     'Monte Everest'
 )
 
-
+criaCartao(
+    'Animais',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+criaCartao(
+    'Música,
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+criaCartao(
+    'História,
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+criaCartao(
+    'Esporte',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+criaCartao(
+    'Música,
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+criaCartao(
+    'Animais',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
 
 
 
